5c586d418a12058db47295ce30861c868419b931
Oleg <ol3g.zor1n@yandex.ru>
2021-12-21 22:09:48 +0300

    intial

--- samples/main_polinom.cpp
+++ samples/main_polinom.cpp
@@ -2,4 +2,15 @@
 
 void main()
 {
+	Polinom A, B, res;
+	A.str_to_poly("x - y");
+	B.str_to_poly("-x + y");
+
+	res = A * B;
+	A += B;
+
+	A.Print();
+	res.Print();
+
+	std::cout << "end" << std::endl;
 }
